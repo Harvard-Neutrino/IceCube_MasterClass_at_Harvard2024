c660ed723ff45cf829ce1643732fdcbf6f64ac62
kcarloni <kcarloni@g.harvard.edu>
2024-04-23 10:33:39 -0400

    setup?

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ URL = "https://github.com/kcarloni/IceCube_MasterClass_at_Harvard2024"
 setup(
     name=NAME,
     url=URL,
-    packages=find_packages()
+    packages=find_packages(),
+    setup_requires=["setuptools>=34.4.0"],
 )
