1ec0380a0b1b09bd58a61e8355f19b129c16b172
kcarloni <kcarloni@g.harvard.edu>
2024-04-23 10:33:20 -0400

    setup?

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+from setuptools import setup, find_packages
 
 NAME = "I3MasterClass"
 URL = "https://github.com/kcarloni/IceCube_MasterClass_at_Harvard2024"
+
+setup(
+    name=NAME,
+    url=URL,
+    packages=find_packages()
+)
