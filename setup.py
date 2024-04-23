1606f2bb209be89be5f38bdb6d5e55479c43e27d
kcarloni <kcarloni@g.harvard.edu>
2024-04-23 11:18:02 -0400

    fix setup?

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup, find_packages
 NAME = "I3MasterClassHarvard2024"
 URL = "https://github.com/kcarloni/IceCube_MasterClass_at_Harvard2024"
 
+print(find_packages())
+
 setup(
     name=NAME,
     url=URL,
-    packages=find_packages(),
+    packages=find_packages(where='./src/'),
+    install_requires=[],
     setup_requires=["setuptools>=34.4.0"],
 )
